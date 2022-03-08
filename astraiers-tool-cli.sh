3d9d7900ccfabceebb5f8cc39571521faf19ef12
Thanhntmany <thanhntmany@gmail.com>
2022-03-08 23:50:01 +0700

    cau truc co ban

--- astraiers-tool-cli.sh
+++ astraiers-tool-cli.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Load configuarations
+source ./config/default.conf
+
+# Simple action: Show banner
 source ./tools/banner.sh
 echo Asstraiers
+echo ${ASS["ss"]}
 
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Installing for this tool
+
