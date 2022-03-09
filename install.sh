d2d03de1952875070023576e7d04d01873ca39f6
Thanhntmany <thanhntmany@gmail.com>
2022-03-09 20:52:24 +0700

    [WIP]

--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@
 
 # Installing for this tool
 
+# Load config
+source ./config/default.conf
+
+# Add command to shell
+echo ${ASSTRAIERS_CLI_NAME}
+ln -fs "astraiers-tool-cli.sh" ${ASSTRAIERS_CLI_NAME}
+
+
