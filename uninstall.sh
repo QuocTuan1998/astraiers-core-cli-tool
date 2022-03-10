ef8357b39a14ca667d98dff852f2ca8ec66ee080
Thanhntmany@realme_note_9 <thanhntmany@gmail.com>
2022-03-10 16:15:13 +0700

    add unintall tool

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+read -r -p "Are you sure you want to remove ass? [y/N] " confirmation
+if [ "$confirmation" != y ] && [ "$confirmation" != Y ]; then
+	  echo "Uninstall cancelled"
+	    exit
+fi
+
+
+ASS_CLI_PATH=$(which ass 2>/dev/null)
+ASS_CORE_DIR=$(dirname $(realpath $$ASS_CLI_PATH))
+
+echo "Removing \"ass\" from commandline."
+rm -f $ASS_CLI_PATH
+echo "Removing core directory."
+rm -Rf $ASS_CORE_DIR
+
+echo "Thanks for trying out Asstraiers's CLI tool (ass). It's been uninstalled."
+echo "Don't forget to restart your terminal!"
+
