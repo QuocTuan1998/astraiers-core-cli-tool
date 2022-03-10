45ee07fbe72072a38a8aec7997e7c7e00a902a0a
Thanhntmany@realme_note_9 <thanhntmany@gmail.com>
2022-03-10 13:06:51 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,32 @@
 
 set -e
 
+echo -e "\nInstalling Astraitors core cli tool..\n\n"
 # Default settings
-ASS_REPO=${REPO:-ohmyzsh/ohmyzsh}
-ASS_REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-ASS_BRANCH=${BRANCH:-master}
+ASS_GIT_USERNAME=${ASS_GIT_USERNAME:-thanhntmany}
+ASS_REPO_NAME=${ASS_REPO:-astraiers-core-cli-tool}
+ASS_REMOTE=${ASS_REMOTE:-https://github.com/${ASS_GIT_USERNAME}/${ASS_REPO_NAME}.git}
 
 PREFIX=${PREFIX:-"/usr"}
 BINDIR=${BINDIR:-$PREFIX/bin}
 LIBEXECDIR=${LIBEXEC:-$PREFIX/libexec}
 
+pushd "$LIBEXECDIR" > /dev/null
+rm -Rf $ASS_REPO_NAME &> /dev/null
+git clone $ASS_REMOTE 
+cd $ASS_REPO_NAME
+
+echo -e "\nAdd \"ass\" to commandline."
+chmod +x astraiers-tool-cli.sh
+ln -f astraiers-tool-cli.sh ass
+
+
+popd > /dev/null
+
 #echo $PREFIX
 #echo $BINDIR
 #echo $LIBEXECDIR
 
-echo "Cloning Asstraiters-core-cli-tool...:
+
+
 
