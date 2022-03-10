f11f2d049d994b2c114cb3bd0e7050fcb684ee18
Thanhntmany@realme_note_9 <thanhntmany@gmail.com>
2022-03-10 16:37:21 +0700

    [WIP] debug issue of uninstall tool

--- uninstall.sh
+++ uninstall.sh
@@ -12,9 +12,11 @@ ASS_CLI_PATH=$(which ass 2>/dev/null)
 ASS_CORE_DIR=$(dirname $(realpath $$ASS_CLI_PATH))
 
 echo "Removing \"ass\" from commandline."
-rm -f $ASS_CLI_PATH
+echo rm -f $ASS_CLI_PATH
+#rm -f $ASS_CLI_PATH
 echo "Removing core directory."
-rm -Rf $ASS_CORE_DIR
+echo rm -Rf $ASS_CORE_DIR
+#rm -Rf $ASS_CORE_DIR
 
 echo "Thanks for trying out Asstraiers's CLI tool (ass). It's been uninstalled."
 echo "Don't forget to restart your terminal!"
