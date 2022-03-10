1176bd1ce87be2697eebc7da28b8c245110fafa7
Thanhntmany@realme_note_9 <thanhntmany@gmail.com>
2022-03-10 12:58:58 +0700

    fx symbolic link issue

--- astraiers-tool-cli.sh
+++ astraiers-tool-cli.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Make sure this script run at right directory. Also when it is run from a symbolic link.
+cd $(dirname -- "$(readlink -f -- "$0")")
+
 # Load configuarations
 source ./config/default.conf
 
