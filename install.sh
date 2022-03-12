9bfceccff0ad9bccb5a05b7f85a67e4b7a9fb9f2
Thanhntmany@realme_note_9-DebianOS <thanhntmany@gmail.com>
2022-03-12 12:18:46 +0700

    Install.sh: fix loi permission khi install tren cac debian-base linux distrobutions
    Tren mot so distro cua debian co set san bien moi truong LIBDIR neen khi run install khong lay dung dia chi bin directory, gay ra loi permission denied

--- install.sh
+++ install.sh
@@ -10,24 +10,23 @@ ASS_REPO_NAME=${ASS_REPO:-astraiers-core-cli-tool}
 ASS_REMOTE=${ASS_REMOTE:-https://github.com/${ASS_GIT_USERNAME}/${ASS_REPO_NAME}.git}
 ASS_BRANCH_NAME=${ASS_BRANCH_NAME:-'prod'}
 
-PREFIX=${PREFIX:-"/usr"}
-BINDIR=${BINDIR:-$PREFIX/bin}
-LIBDIR=${LIBDIR:-$PREFIX/lib}
+_PREFIX=${PREFIX:-"/usr"}
+_BINDIR=${_BINDIR:-${_PREFIX}/bin}
+_LIBDIR=${_LIBDIR:-$_PREFIX/lib}
 
-pushd "$LIBDIR" > /dev/null
+cd "$_LIBDIR"
 rm -Rf $ASS_REPO_NAME &> /dev/null
-git clone -b $ASS_BRANCH_NAME $ASS_REMOTE 
+echo git clone -b $ASS_BRANCH_NAME $ASS_REMOTE
+git clone -b $ASS_BRANCH_NAME $ASS_REMOTE
 
 cd $ASS_REPO_NAME
 ASS_WORKING_DIR=$PWD
 
 echo -e "\nAdd \"ass\" to commandline."
 chmod +x astraiers-tool-cli.sh
-TMP_ASS_RELATIVE_PATH=$(realpath --relative-to="${BINDIR}" ./astraiers-tool-cli.sh)
-cd $BINDIR
+TMP_ASS_RELATIVE_PATH=$(realpath --relative-to="${_BINDIR}" ./astraiers-tool-cli.sh)
+cd $_BINDIR
 ln -sf "$TMP_ASS_RELATIVE_PATH" ass
-
-popd > /dev/null
 
 echo -e "Done.\n"
 
